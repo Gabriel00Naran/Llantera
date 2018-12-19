a5052d7bbbe7e5404b472102e03e075a0c55f8da
Gabriel00Naran <gaguirref.itsbj@gmail.com>
2018-12-19 05:15:33 -0500

    cambios a proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavegadorComponent } from './navegador/navegador.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { BarraComponent } from './barra/barra.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavegadorComponent
+    NavegadorComponent,
+    CuerpoComponent,
+    BarraComponent
   ],
   imports: [
     BrowserModule,
