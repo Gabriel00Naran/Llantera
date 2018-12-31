f4b05f7d02f29785aafd0d632c5f641d1b10bbf8
Gabriel00Naran <gaguirref.itsbj@gmail.com>
2018-12-30 19:52:22 -0500

    interfaz base creada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { CuerpoComponent } from './cuerpo/cuerpo.component';
 import { Pagina2Component } from './pagina2/pagina2.component';
 import { EquivalenciasComponent } from './equivalencias/equivalencias.component';
+import { NavegadorComponent} from './componentes/navegador/navegador.component'
 import { ResultadosComponent } from './resultados/resultados.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -10,9 +11,8 @@ const routes: Routes = [
   { path: 'cuerpo', component: CuerpoComponent },
   { path: 'pagina2', component: Pagina2Component },
   { path: 'equivalencias', component: EquivalenciasComponent },
-  { path: 'resultados', component: ResultadosComponent }
-
-
+  { path: 'resultados', component: ResultadosComponent },
+  { path: 'navegador', component: NavegadorComponent },
 ];
 
 @NgModule({
